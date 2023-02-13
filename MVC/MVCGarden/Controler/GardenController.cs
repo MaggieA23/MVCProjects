28de772214a2954c51c89e526f9bd9a935f81b26
MaggieA23 <m.georgieva2019@pmg-pernik.com>
2023-02-13 11:30:21 +0200

    add controler

---
+++ MVC/MVCGarden/Controler/GardenController.cs
@@ -0,0 +1,23 @@
+﻿using MVCGarden.Model;
+using MVCGarden.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGarden.Controler
+{
+    public class GardenController
+    {
+        private Display displays;
+        private Garden gardens;
+        public GardenController()
+        {
+            displays = new Display();
+            gardens = new Garden(displays.PriceVegetable,displays.PriceFruit,displays.KgVegetable,displays.KgFruit);
+            displays.Total = gardens.CalculateSum();
+            displays.ShowResult();
+        }
+    }
+}
