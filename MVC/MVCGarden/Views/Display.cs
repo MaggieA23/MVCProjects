11f5e17474da3408b7c7a6589baf4ee522e4f35d
MaggieA23 <m.georgieva2019@pmg-pernik.com>
2023-02-13 11:18:01 +0200

    add view

---
+++ MVC/MVCGarden/Views/Display.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGarden.Views
+{
+   public  class Display
+    {
+        public Display()
+        {
+            this.PriceVegetable = 0;
+           this.PriceFruit = 0;
+            this.KgVegetable = 0;
+            this.KgFruit = 0;
+            this.GetValue();
+        }
+
+        public decimal PriceVegetable { get; set; }
+        public decimal PriceFruit { get; set; }
+        public int KgVegetable { get; set; }
+        public int KgFruit { get; set; }
+        public decimal Total { get; set; }
+        public void GetValue()
+        {
+            this.PriceVegetable = decimal.Parse(Console.ReadLine());
+            this.PriceFruit = decimal.Parse(Console.ReadLine());
+            this.KgVegetable = int.Parse(Console.ReadLine());
+            this.KgFruit= int.Parse(Console.ReadLine());
+        }
+        public void ShowResult()
+        {
+            Console.WriteLine(this.Total);
+        }
+    }
+}
