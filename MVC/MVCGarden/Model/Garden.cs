0f6b04a6ed8f91990eacb903b74cf1121050525b
MaggieA23 <m.georgieva2019@pmg-pernik.com>
2023-02-13 11:01:30 +0200

    add model

---
+++ MVC/MVCGarden/Model/Garden.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCGarden.Model
+{
+    public class Garden
+    {
+        public Garden(decimal priceVegetable, decimal priceFruit, int kgVegetable, int kgFruit)
+        {
+            this.PriceVegetable = priceVegetable;
+            this.PriceFruit = priceFruit;
+            this.KgVegetable = kgVegetable;
+            this.KgFruit = kgFruit;
+        }
+
+        public decimal PriceVegetable { get; set; }
+        public decimal PriceFruit { get; set; }
+        public int KgVegetable { get; set; }
+        public int KgFruit { get; set; }
+        public decimal CalculateSum()
+        {
+            return 1.94m * (this.KgFruit * this.PriceFruit + this.KgVegetable * this.PriceVegetable);
+        }
+    }
+}
